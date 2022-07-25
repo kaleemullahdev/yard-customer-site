a26874b12d07e2b9012bf458b485a26ddc8e0c92
kaleemullahdev <kaleemullah786.ku61@gmail.com>
2022-07-26 00:16:03 +0500

    adds links fix

--- src/components/CustomerSite/components/NavMenu.js
+++ src/components/CustomerSite/components/NavMenu.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Menu from "@material-ui/core/Menu";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 import { StyledIconButton, StyledMenuItem } from "./elements";
-import { Link } from "gatsby"
+import { Link } from "gatsby";
 export const NavMenu = () => {
   const [anchorEl, setAnchorEl] = useState(null);
   const handleClose = () => {
@@ -32,34 +32,22 @@ export const NavMenu = () => {
         onClose={handleClose}
       >
         <StyledMenuItem onClick={handleClose}>
-          <Link replace href="/customersite/services/year-round">
-            Year Round Maintenance
-          </Link>
+          <Link to="/services/year-round">Year Round Maintenance</Link>
         </StyledMenuItem>
         <StyledMenuItem onClick={handleClose}>
-          <Link replace href="/customersite/services/lawn-moving">
-            Lawn Moving
-          </Link>
+          <Link to="/services/lawn-moving">Lawn Moving</Link>
         </StyledMenuItem>
         <StyledMenuItem onClick={handleClose}>
-          <Link replace href="/customersite/services/weed-control">
-            Weed Control
-          </Link>
+          <Link to="/services/weed-control">Weed Control</Link>
         </StyledMenuItem>
         <StyledMenuItem onClick={handleClose}>
-          <Link replace href="/customersite/services/spring-clean-up">
-            Spring Cleanup
-          </Link>
+          <Link to="/services/spring-clean-up">Spring Cleanup</Link>
         </StyledMenuItem>
         <StyledMenuItem onClick={handleClose}>
-          <Link replace href="/customersite/services/fall-clean-up">
-            Fall Cleanup
-          </Link>
+          <Link to="/services/fall-clean-up">Fall Cleanup</Link>
         </StyledMenuItem>
         <StyledMenuItem onClick={handleClose}>
-          <Link replace href="/customersite/services/snow-removal">
-            Snow Removal
-          </Link>
+          <Link to="/services/snow-removal">Snow Removal</Link>
         </StyledMenuItem>
       </Menu>
     </>
